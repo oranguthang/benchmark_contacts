1a1b93f9a5dd986e5a2431d3cf3ff2c59d60b9e4
Daniel Oranguthang <75395800+oranguthang@users.noreply.github.com>
2025-05-12 01:34:27 +0300

    Database

--- python_aiohttp/app/main.py
+++ python_aiohttp/app/main.py
@@ -1,6 +1,5 @@
 from aiohttp import web
 from routes import create_contact, get_contacts, ping
-from db import engine, Base
 
 
 async def init_app():
@@ -8,9 +7,6 @@ async def init_app():
     app.router.add_post('/contacts', create_contact)
     app.router.add_get('/contacts', get_contacts)
     app.router.add_get('/ping', ping)
-
-    async with engine.begin() as conn:
-        await conn.run_sync(Base.metadata.create_all)
 
     return app
 
