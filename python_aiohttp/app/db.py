5110ecc308ca4e764c5bfca4e10f3edebeeecddd
Daniel Dushistov <9932543+danields966@users.noreply.github.com>
2025-05-11 18:30:52 +0300

    Add Python project

---
+++ python_aiohttp/app/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+import os
+
+DATABASE_URL = os.getenv("DATABASE_URL").replace("postgres://", "postgresql+asyncpg://")
+
+engine = create_async_engine(DATABASE_URL, echo=False, future=True)
+SessionLocal = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+Base = declarative_base()
