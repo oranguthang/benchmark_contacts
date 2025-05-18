41388b4571f9c977ba4bd4a3527f1624d64bec13
Daniel Oranguthang <75395800+oranguthang@users.noreply.github.com>
2025-05-19 00:35:04 +0300

    PHP

--- php_slim/app/index.php
+++ php_slim/app/index.php
@@ -2,22 +2,24 @@
 require 'vendor/autoload.php';
 
 use Slim\Factory\AppFactory;
+use DI\Container;
 
+$container = new Container();
+AppFactory::setContainer($container);
 $app = AppFactory::create();
 
-$container = $app->getContainer();
 $container->set('db', function() {
     return new PDO('pgsql:host=python_db;dbname=postgres', 'postgres', 'postgres');
 });
 
-$app->post('/contacts', function ($request, $response) {
+$app->post('/contacts', function ($request, $response) use ($container) {
     $data = $request->getParsedBody();
-    $stmt = $this->get('db')->prepare('INSERT INTO contacts (external_id, phone_number) VALUES (?, ?)');
+    $stmt = $container->get('db')->prepare('INSERT INTO contacts (external_id, phone_number) VALUES (?, ?)');
     $stmt->execute([$data['external_id'], $data['phone_number']]);
     return $response->withStatus(201);
 });
 
-$app->get('/contacts', function ($request, $response) {
+$app->get('/contacts', function ($request, $response) use ($container) {
     $params = $request->getQueryParams();
     $limit = isset($params['limit']) ? min((int)$params['limit'], 10000) : 10000;
     $offset = isset($params['offset']) ? (int)$params['offset'] : 0;
@@ -38,12 +40,17 @@ $app->get('/contacts', function ($request, $response) {
     $paramsList[] = $limit;
     $paramsList[] = $offset;
 
-    $stmt = $this->get('db')->prepare($sql);
+    $stmt = $container->get('db')->prepare($sql);
     $stmt->execute($paramsList);
     $contacts = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
     $response->getBody()->write(json_encode($contacts));
     return $response->withHeader('Content-Type', 'application/json');
+});
+
+$app->get('/ping', function ($request, $response) {
+    $response->getBody()->write('pong');
+    return $response->withHeader('Content-Type', 'text/plain');
 });
 
 $app->run();
